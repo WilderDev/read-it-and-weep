415391fd572b652cc63a214717d74cba32c9921c
WilderDev <williamjohnwilder@outlook.com>
2022-09-26 18:15:30 -0500

    ADD: Navbar & Project Layout

---
+++ src/app/bookshelf/saved-book-list/saved-book-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-saved-book-list',
+  templateUrl: './saved-book-list.component.html',
+  styleUrls: ['./saved-book-list.component.css']
+})
+export class SavedBookListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
